3825b9404b2317765306e826abd297a81cf0b0f5
TimeBean <alexanderklush8@gmail.com>
2026-08-11 14:07:00 +0300

    refactor: remove reduntant quantizers

--- Service.WebSite.Domain/QuantizationAlgorithm.cs
+++ Service.WebSite.Domain/QuantizationAlgorithm.cs
@@ -12,15 +12,6 @@ public enum QuantizationAlgorithm
 
     [Display(Name = "Linear 8")]
     Linear8,
-
-    [Display(Name = "Linear 16")]
-    Linear16,
-
-    [Display(Name = "Linear 32")]
-    Linear32,
-
-    [Display(Name = "Linear 64")]
-    Linear64,
     
     [Display(Name = "Grayscale (Average)")]
     GrayscaleByAverage,
